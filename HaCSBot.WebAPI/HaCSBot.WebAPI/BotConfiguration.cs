e4c1338cc4ac27df66189d173794326fb91526a2
Ilkham Shakirov <98184014+huml1k@users.noreply.github.com>
2025-12-05 12:09:37 +0300

    Добавлен не рабочий бот

---
+++ HaCSBot.WebAPI/HaCSBot.WebAPI/BotConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace HaCSBot.WebAPI
+{
+    public class BotConfiguration
+    {
+        public string BotToken { get; init; } = default!;
+        public Uri BotWebhookUrl { get; init; } = default!;
+    }
+}
--- HaCSBot.WebAPI/HaCSBot.WebAPI/Program.cs
+++ HaCSBot.WebAPI/HaCSBot.WebAPI/Program.cs
@@ -1,4 +1,8 @@
 
+using HaCSBot.WebAPI.Handlers;
+using Microsoft.AspNetCore;
+using Telegram.Bot;
+
 namespace HaCSBot.WebAPI
 {
     public class Program
@@ -8,6 +12,11 @@ namespace HaCSBot.WebAPI
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            var botConfigSection = builder.Configuration.GetSection("BotConfiguration");
+            builder.Services.Configure<BotConfiguration>(botConfigSection);
+            builder.Services.AddHttpClient("tgwebhook").RemoveAllLoggers().AddTypedClient<ITelegramBotClient>(
+                httpClient => new TelegramBotClient(botConfigSection.Get<BotConfiguration>()!.BotToken, httpClient));
+            builder.Services.AddSingleton<UpdateHandler>();
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -24,7 +33,6 @@ namespace HaCSBot.WebAPI
             }
 
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
             
 
